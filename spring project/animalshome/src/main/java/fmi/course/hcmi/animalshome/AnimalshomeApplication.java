dc9f8344d6abf97ee90837eaf992ed6626e67663
Rositsa98 <admin@localhost.localdomain>
2020-03-22 16:55:02 +0200

    Add spring project

---
+++ spring project/animalshome/src/main/java/fmi/course/hcmi/animalshome/AnimalshomeApplication.java
@@ -0,0 +1,13 @@
+package fmi.course.hcmi.animalshome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalshomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalshomeApplication.class, args);
+	}
+
+}
